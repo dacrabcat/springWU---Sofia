a3b1d248a9b2a35ebe42cdaa4adfdb8d4e50e597
dacrabcat <xinyuan.andersson@gmail.com>
2026-03-06 14:22:26 +0100

    Ny sida och funggerande navknapp

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-document.querySelector("#fade-layer").addEventListener("click",showMenu)
+document.querySelector(".fade-layer").addEventListener("click",showMenu)
 
 document.querySelector("#button").addEventListener("click",showMenu)
 
 function showMenu(){
-    let menu = document.querySelector("#mobilenav");
+    let menu = document.querySelector(".mobilenav");
     menu.classList.toggle("show");
 
-    let layer = document.querySelector("#fade-layer");
+    let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
 }
